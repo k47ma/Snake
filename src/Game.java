fd99ece75478841a9a64c7d69c64bfbcdcd1a7bd
Kai Ma <k47ma@edu.uwaterloo.ca>
2017-07-25 19:57:35 -0400

    - Finished main skeleton for snake game

--- src/Game.java
+++ src/Game.java
@@ -1,24 +1,114 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-/**
- * Created by mk on 2017/7/25.
- */
-public class Game {
+
+public class Game extends JFrame {
+    private JFrame mainFrame;
+    private JPanel gridContainer;
+    private JPanel[] grid;
+    
+    private int WIDTH = 20;
+    private int HEIGHT = 20;
+    
+    private int S_LENGTH;
+    private String S_DIRECTION;
+    private Point[] SNAKE;
+
+    private int DELAY = 100;
+
+    private Game() {
+        setUpGame();
+    }
+
     public static void main(String [] args) {
-        JFrame frame = new JFrame("Hello world!");
-        JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout());
+        Game game = new Game();
+        game.startGame();
+    }
 
-        JLabel label = new JLabel("This is a label!");
-        JButton button = new JButton("Button");
+    private void setUpGame() {
+        mainFrame = new JFrame("Snake");
+        gridContainer = new JPanel();
+        grid = new JPanel[WIDTH * HEIGHT];
+        SNAKE = new Point[WIDTH * HEIGHT];
 
-        panel.add(label);
-        panel.add(button);
+        // add panels to grid
+        gridContainer.setLayout(new GridLayout(WIDTH, HEIGHT, 5, 5));
+        for (int i = 0; i < WIDTH * HEIGHT; ++i) {
+            JPanel panel = new JPanel();
+            panel.setBackground(Color.white);
+            gridContainer.add(panel);
+            grid[i] = panel;
+        }
 
-        frame.add(panel);
-        frame.setSize(300, 300);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        mainFrame.add(gridContainer);
+        mainFrame.setSize(1000, 1000);
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainFrame.setVisible(true);
+    }
+
+    private void startGame() {
+        S_LENGTH = 3;
+        S_DIRECTION = "right";
+        SNAKE = new Point[WIDTH * HEIGHT];
+        
+        for (int i = 0; i < WIDTH * HEIGHT; ++i) {
+            SNAKE[i] = new Point(-1, -1);
+        }
+
+        SNAKE[0].x = 11;
+        SNAKE[0].y = 10;
+        SNAKE[1].x = 10;
+        SNAKE[1].y = 10;
+        SNAKE[2].x = 9;
+        SNAKE[2].y = 10;
+
+        drawSnake();
+
+        Timer timer = new Timer(DELAY, null);
+        timer.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                moveSnake();
+                drawSnake();
+            }
+        });
+        timer.setRepeats(true);
+        timer.start();
+    }
+
+    private void moveSnake() {
+        for (int i = 0; i < S_LENGTH; ++i) {
+            SNAKE[i].x += 1;
+
+            if (SNAKE[i].x > WIDTH) {
+                SNAKE[i].x = 1;
+            }
+        }
+    }
+
+    private void drawSnake() {
+        clearBoard();
+        for (int i = 0; i < S_LENGTH; ++i) {
+            int x = SNAKE[i].x;
+            int y = SNAKE[i].y;
+
+            if (i == 0) {
+                grid[xyToInd(x, y)].setBackground(Color.green);
+            } else {
+                grid[xyToInd(x, y)].setBackground(Color.gray);
+            }
+        }
+    }
+
+    private int xyToInd (int x, int y) {
+        return (y - 1) * WIDTH + x - 1;
+    }
+
+    private void clearBoard() {
+        for (int i = 0; i < WIDTH * HEIGHT; ++i) {
+            grid[i].setBackground(Color.white);
+        }
     }
 }
